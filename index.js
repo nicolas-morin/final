a30a5a23799a929c78de6ee6bfc30723a87f1b25
Nicolas Morin <nicolas.morin@kellogg.northwestern.edu>
2021-03-13 16:14:15 -0600

    Login + other functions

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-firebase.auth().onAuthStateChanged(async function(user) {
+firebase.auth().onAuthStateChanged(async function (user) {
   if (user) {
     // Signed in
-    console.log('signed in')
+    console.log("signed in");
+    db.collection("users").doc(user.uid).set({
+      name: user.displayName,
+      email: user.email,
+    });
+    document.querySelector(".sign-in-or-sign-out").innerHTML = `
+    <button class="text-pink-500 underline sign-out">Sign Out</button>
+    `;
+    document
+      .querySelector(".sign-out")
+      .addEventListener("click", function (event) {
+        console.log("sign out clicked");
+        fire.aut().signOut();
+        document.location.href = "index.html";
+      });
   } else {
     // Signed out
-    console.log('signed out')
-
+    console.log("signed out");
     // Initializes FirebaseUI Auth
-    let ui = new firebaseui.auth.AuthUI(firebase.auth())
-
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
     // FirebaseUI configuration
     let authUIConfig = {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: 'index.html'
-    }
-
+      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+      signInSuccessUrl: "index.html",
+    };
     // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
+    ui.start(".sign-in-or-sign-out", authUIConfig);
   }
-})
+});
