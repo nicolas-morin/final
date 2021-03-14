fafdbe027bd317f4991380c9c18bb225c08911df
Nicolas Morin <nicolas.morin@kellogg.northwestern.edu>
2021-03-13 19:13:29 -0600

    Fixed database, added html

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 firebase.auth().onAuthStateChanged(async function (user) {
+  let db = firebase.firestore();
+  console.log(user);
   if (user) {
     // Signed in
     console.log("signed in");
@@ -30,3 +32,23 @@ firebase.auth().onAuthStateChanged(async function (user) {
     ui.start(".sign-in-or-sign-out", authUIConfig);
   }
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
